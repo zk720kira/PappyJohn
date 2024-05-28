92ed0d452df23f86a69c3538f796732a944945cd
zk720kira <totozk720@gmail.com>
2024-05-28 13:25:13 +0200

    Modify python installation

---
+++ InitialisationServer.bat
@@ -0,0 +1,36 @@
+@echo off
+cls
+
+echo Des installation sont effectuees dans ce script. Veuillez l'ouvrir en Administrateur.
+echo Si vous n'etes pas en Administrateur veuillez fermer cette fenetre et relancer le script en Administrateur.
+echo Si non pressez sur Enter pour continuer.
+
+PAUSE
+
+cd C:\
+
+REM Cloner le dépôt GitHub
+git clone https://github.com/zk720kira/PappyJohn
+
+REM Naviguer vers le répertoire cloné
+cd C:\PappyJohn
+
+REM Installer les dépendances Python
+python -m ensurepip
+python -m pip install --upgrade pip
+echo Installation des packages requis pour le fonctionnement de l'application.
+pip install -r requirements.txt
+
+REM Installer MySQL
+echo Installation de MySQL.
+echo Cela peux prendre quelques minutes.
+winget install Oracle.MySQL
+
+REM Créer la base de données
+mysql --user=root --skip-password --execute="CREATE DATABASE IF NOT EXISTS ruegg_thomas_expi1b_pappy_john"
+
+REM Importer le fichier dump SQL
+mysql --user=root --skip-password ruegg_thomas_expi1b_pappy_john < database\ruegg_thomas_epxi1b_dump.sql
+
+echo Installation terminee, veuillez presser sur Enter.
+PAUSE
